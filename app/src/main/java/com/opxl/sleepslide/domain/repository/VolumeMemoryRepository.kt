3c2e417b4af54b0add57290cbf57a7c480d33f2c
Chibueze  felix <33674336+leadpresence@users.noreply.github.com>
2026-08-24 21:06:49 +0100

    Impl(repo): Implement  VolumeMemoryRepository int
    This handles interface for VolumeMemoryRepository and all volume states and set ups

---
+++ app/src/main/java/com/opxl/sleepslide/domain/repository/VolumeMemoryRepository.kt
@@ -0,0 +1,20 @@
+package com.opxl.sleepslide.domain.repository
+
+import kotlinx.coroutines.flow.Flow
+
+interface VolumeMemoryRepository {
+
+    fun observeVolume(soundId: String): Flow<Float>
+
+    suspend fun getVolume(soundId: String): Float
+
+    suspend fun getVolumesForSounds(soundIds: List<String>): Map<String, Float>
+
+    suspend fun saveVolume(soundId: String, volume: Float)
+
+    suspend fun saveVolumes(volumes: Map<String, Float>)
+
+    suspend fun clearVolume(soundId: String)
+
+    suspend fun clearAll()
+}
