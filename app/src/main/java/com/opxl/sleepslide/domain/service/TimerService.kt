4af45605985cdeba4e1562263377b553e4006416
Chibueze  felix <33674336+leadpresence@users.noreply.github.com>
2026-08-25 04:31:10 +0100

    Impl(repo): Implemented  the   TimerService
    This enables viewmodels to observe the state of the timer and react accordingly

--- app/src/main/java/com/opxl/sleepslide/domain/service/TimerService.kt
+++ app/src/main/java/com/opxl/sleepslide/domain/service/TimerService.kt
@@ -1,4 +1,17 @@
 package com.opxl.sleepslide.domain.service
 
+import com.opxl.sleepslide.domain.model.Domain
+import kotlinx.coroutines.flow.StateFlow
+
 interface TimerService {
+
+    val timerState: StateFlow<Domain.TimerState>
+
+    suspend fun start(durationMs: Long, fadeBeforeEndMs: Long = 60_000L)
+
+    suspend fun cancel()
+
+    suspend fun addTime(extraMs: Long)
+
+    suspend fun reduceTime(reduceMs: Long)
 }
