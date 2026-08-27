e6d27525e011e8da0e9bb6c9bdb67f711afa1059
Chibueze  felix <33674336+leadpresence@users.noreply.github.com>
2026-08-27 15:01:11 +0100

    Feat(repo): MainActivity
    Added Service connection to bind the AudioServiceImpl on start of the application

--- app/src/main/java/com/opxl/sleepslide/MainActivity.kt
+++ app/src/main/java/com/opxl/sleepslide/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.opxl.sleepslide
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,9 +16,26 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.opxl.sleepslide.data.audio.AudioServiceImpl
 import com.opxl.sleepslide.ui.theme.SleepSlideTheme
 
 class MainActivity : ComponentActivity() {
+
+    // In MainActivity or a ServiceConnectionManager
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName, binder: IBinder) {
+            val service = (binder as AudioServiceImpl.LocalBinder).getService()
+            // pass to ViewModel or a shared holder
+        }
+        override fun onServiceDisconnected(name: ComponentName) {}
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Intent(this, AudioServiceImpl::class.java).also {
+            bindService(it, connection, Context.BIND_AUTO_CREATE)
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
