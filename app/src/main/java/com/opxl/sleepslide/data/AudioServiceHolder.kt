077b0080318d6ebcc4b81bb86c1ae6431e8980f9
Chibueze  felix <33674336+leadpresence@users.noreply.github.com>
2026-08-31 09:05:01 +0100

    Fix(repo): Reverted to stateflow

--- app/src/main/java/com/opxl/sleepslide/data/AudioServiceHolder.kt
+++ app/src/main/java/com/opxl/sleepslide/data/AudioServiceHolder.kt
@@ -2,7 +2,6 @@ package com.opxl.sleepslide.data
 
 
 import com.opxl.sleepslide.domain.service.AudioService
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -21,7 +20,7 @@ import javax.inject.Singleton
 class AudioServiceHolder @Inject constructor() {
 
     private val _service = MutableStateFlow<AudioService?>(null)
-    val service: Flow<AudioService?> = _service.asStateFlow()
+    val service: StateFlow<AudioService?> = _service.asStateFlow()
 
     val current: AudioService? get() = _service.value
 
